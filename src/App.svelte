1ef8c104d12b709b12ca041c1160da8ca695cdb9
Guhaprasaanth Nandagopal <guhan.delta@gmail.com>
2020-10-02 16:54:21 -0400

    Used loop to render a component for every value in an array, by passing the values as props/args

--- src/App.svelte
+++ src/App.svelte
@@ -1,34 +1,30 @@
 <script>
-	let name = "Aattakaaran";
-	let points = 100;
-	// const addPoint = () => (points += 1);
-	const reducePoint = () => (points -= 1);
-	let showModifiers = false;
-	const toggleModifiers = () => (showModifiers = !showModifiers);
+	import Navbar from './Navbar.svelte'
+	import Player from './Player.svelte'
+
+	const players = [
+		{
+			name: "Sachin Tendulkar",
+			points: 90	
+		},
+		{
+			name: "Mahinder Singh Dhoni",
+			points: 97	
+		},
+		{
+			name: "Virat Kholi",
+			points: 88	
+		},
+	];
 </script>
 
-<style>
-	h1 {
-		color: purple;
-	}
-	h3 {
-		margin-bottom: 10px;
-		border-bottom: 2px solid #000;
-	}
-</style>
+<Navbar />
 <div class="container">
-	<div class="card">
-		<h1>
-			{name}
-			<button class="btn btn-sm" on:click={toggleModifiers}>
-				{#if showModifiers}-{:else}+{/if}
-			</button>
-		</h1>
-		<h3>Points: {points}</h3>
-		{#if showModifiers}
-			<button class="btn" on:click={() => (points += 1)}>Add Points</button>
-			<button class="btn btn-dark" on:click={reducePoint}>Reduce Points</button>
-			<input type="number" bind:value={points} />
-		{/if}
-	</div>
+	{#if players.length === 0}
+		<p>No Players Available</p>
+	{:else}
+		{#each players as player }
+			<Player name={player.name} points={player.points} />
+		{/each}
+	{/if}
 </div>
---
+++ src/Player.svelte
@@ -0,0 +1,32 @@
+<script>
+	export let name; //Instead of setting the values, get the values as/from the props
+	export let points;
+	// const addPoint = () => (points += 1);
+	const reducePoint = () => (points -= 1);
+	let showModifiers = false;
+	const toggleModifiers = () => (showModifiers = !showModifiers);
+</script>
+
+<style>
+	h1 {
+		color: purple;
+	}
+	h3 {
+		margin-bottom: 10px;
+		border-bottom: 2px solid #000;
+	}
+</style>
+<div class="card">
+	<h1>
+		{name}
+		<button class="btn btn-sm" on:click={toggleModifiers}>
+			{#if showModifiers}-{:else}+{/if}
+		</button>
+	</h1>
+	<h3>Points: {points}</h3>
+	{#if showModifiers}
+		<button class="btn" on:click={() => (points += 1)}>Add Points</button>
+		<button class="btn btn-dark" on:click={reducePoint}>Reduce Points</button>
+		<input type="number" bind:value={points} />
+	{/if}
+</div>
